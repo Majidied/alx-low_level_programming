6e01f09475aca45e3561034762468392a486d075
Majidied <majidimajidi2003@gmail.com>
2023-08-10 17:59:32 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
 
 /*
  * main - Entry point
- * Description: prints "Programming is like building a multilingual puzzle"
+ * Description - prints Programming is like building a multilingual puzzle
  * Return: Always (0)
  */
 
