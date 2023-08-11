d6be5012b547c3d793ba75d52404af078b2141be
Majidied <majidimajidi2003@gmail.com>
2023-08-10 17:18:25 -0700

    langage c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /* using the fonction puts to print a message */ 
 int main (void)
 {
-	puts("Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
