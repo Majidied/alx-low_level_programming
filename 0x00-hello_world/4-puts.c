87664b9424c67cf4ba5939b43685740a4dabf780
Majidied <majidimajidi2003@gmail.com>
2023-08-10 17:02:05 -0700

    langage c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* using the fonction puts to print a message */ 
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle \n");
+	return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* prinf a message using prinf */
+int main (void) 
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n ");
+	return 0;
+}
