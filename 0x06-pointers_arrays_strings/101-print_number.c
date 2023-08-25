ef3f8ae5dd5bb1f31c83a58c55811fdcbfd3e756
Majidied <majidimajidi2003@gmail.com>
2023-08-24 19:22:48 -0700

    advenced tasks

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_number - Prints any integer with putchar
+  * @n: Number to prints
+  *
+  * Return: Nothing
+  */
+void print_number(int n)
+{
+	unsigned int k;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	k = n;
+
+	if (k / 10)
+		print_number(k / 10);
+
+	_putchar(k % 10 + '0');
+}
